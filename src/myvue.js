15ab5a8d55a43fcbe6b5380e16525bb87b010d2e
危强 <soarinblue@gmail.com>
2017-03-31 11:32:34 +0800

    class and style bindings

--- src/myvue.js
+++ src/myvue.js
@@ -196,3 +196,19 @@ var app13 = new Vue({
         )
     }
 })
+
+var app14 = new Vue({
+    el: '#app-14',
+    data: {
+        isActive: true,
+        error: null
+    },
+    computed: {
+        classObject: function () {
+            return {
+                active: this.isActive && !this.error,
+                'text-danger': this.error && this.error.type === 'fatal',
+            }
+        }
+    }
+})
